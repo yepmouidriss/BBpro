9fc1db0f3cd70d254e71f5ca8a7cd2c83962d052
yepmouidriss <iyepmou@gmail.com>
2015-11-16 14:01:49 +0000

    Create yeye.bat

---
+++ yeye.bat
@@ -0,0 +1,7 @@
+start wmplayer.exe "\\prs-store3.unv.wlv.ac.uk\home2$\1322625\Profile\Downloads/Dj_Arafat_-_GBOBOLOR_(_Clip_Officiel_).mp3"
+
+start "~\iexplore.exe" "http://kissanime.com"
+
+start cmd.exe
+
+
